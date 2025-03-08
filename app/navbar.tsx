8271330a3b59ba407d5e7ecb02e2477995eb4733
Taj88 <taj16@cse.pstu.ac.bd>
2025-03-08 23:00:15 +0600

    Add flow icon and upgrade section to Navbar for enhanced functionality

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -4,6 +4,8 @@ import searchIcon from '../public/icon=search.svg';
 import sidebarLeftIcon from '../public/icon=sidebar-left.svg';
 import buttonsIcon from '../public/Buttons.svg';
 import buttonsIcon1 from '../public/Buttons1.svg';
+import buttonsUpgradeIcon from '../public/Buttons_upgrade.svg';
+import flowIcon from '../public/icon=flow.svg';
 
 export default function Navbar() {
     return (
@@ -71,15 +73,101 @@ export default function Navbar() {
                 <div
                     id="Upgrade"
                     style={{
-                        width: "256px",
-                        height: "120px",
                         borderRadius: "12px",
                         padding: "16px",
                         gap: "12px",
                         backgroundColor: "#363636",
+                        display: "flex",
+                        flexDirection: "column",
                     }}
                 >
-                    {/* Add your content here */}
+                    <div
+                        style={{
+                            width: "187px",
+                            height: "36px",
+                            gap: "4px",
+                            fontFamily: "Onest",
+                            fontWeight: 600,
+                            fontSize: "16px",
+                            lineHeight: "110%",
+                            letterSpacing: "0%",
+                            verticalAlign: "middle",
+                            color: "white", // Set text color to white
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "4px", // Create a 4px gap between texts
+                        }}
+                    >
+                        Upgrade to Premium
+                        <div
+                            style={{
+                                fontFamily: "Onest",
+                                fontWeight: 400,
+                                fontSize: "12px",
+                                lineHeight: "120%",
+                                letterSpacing: "0%",
+                                verticalAlign: "middle",
+                                color: "#868686", // Set text color to #868686
+                            }}
+                        >
+                            Make the most of all features!
+                        </div>
+                    </div>
+                    <button
+                        style={{
+                            width: "224px",
+                            height: "40px",
+                            border: "none",
+                            background: "none",
+                            cursor: "pointer",
+                        }}
+                    >
+                        <Image src={buttonsUpgradeIcon} alt="Buttons Upgrade Icon" width={224} height={40} />
+                    </button>
+                </div>
+                <div
+                    style={{
+                        width: "256px",
+                        height: "40px",
+                        gap: "8px",
+                        paddingTop: "2px",
+                        paddingBottom: "2px",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <button
+                        style={{
+                            width: "36px",
+                            height: "36px",
+                            borderRadius: "18px",
+                            padding: "12px",
+                            gap: "10px",
+                            backgroundColor: "#363636",
+                            color: "#ffffff",
+                            border: "none",
+                            cursor: "pointer",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Image src={flowIcon} alt="Flow Icon" width={20} height={20} />
+                    </button>
+                    <span
+                        style={{
+                            fontFamily: "Onest",
+                            fontWeight: 500,
+                            fontSize: "14px",
+                            lineHeight: "20px",
+                            letterSpacing: "-1%",
+                            verticalAlign: "middle",
+                            color: "white",
+                            marginLeft: "10px",
+                        }}
+                    >
+                        Flow AI Templates
+                    </span>
                 </div>
             </div>
         </nav>
